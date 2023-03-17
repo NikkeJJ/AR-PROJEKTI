10ef5121196de9015afccaac007100c901e3e7e2
NikkeJJ <99720132+NikkeJJ@users.noreply.github.com>
2023-03-17 19:10:41 +0200

    Koko projekti

---
+++ AR Project/ARPROJEKTI2_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,125 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 UnityEngine.GameObject AnchorCreator::get_AnchorPrefab()
+extern void AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A (void);
+// 0x00000002 System.Void AnchorCreator::set_AnchorPrefab(UnityEngine.GameObject)
+extern void AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF (void);
+// 0x00000003 System.Void AnchorCreator::RemoveAllAnchors()
+extern void AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21 (void);
+// 0x00000004 System.Void AnchorCreator::Awake()
+extern void AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E (void);
+// 0x00000005 System.Void AnchorCreator::Update()
+extern void AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0 (void);
+// 0x00000006 System.Void AnchorCreator::.ctor()
+extern void AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E (void);
+// 0x00000007 System.Void AnchorCreator::.cctor()
+extern void AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4 (void);
+// 0x00000008 System.Single ARFeatheredPlaneMeshVisualizer::get_featheringWidth()
+extern void ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D (void);
+// 0x00000009 System.Void ARFeatheredPlaneMeshVisualizer::set_featheringWidth(System.Single)
+extern void ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110 (void);
+// 0x0000000A System.Void ARFeatheredPlaneMeshVisualizer::Awake()
+extern void ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA (void);
+// 0x0000000B System.Void ARFeatheredPlaneMeshVisualizer::OnEnable()
+extern void ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0 (void);
+// 0x0000000C System.Void ARFeatheredPlaneMeshVisualizer::OnDisable()
+extern void ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8 (void);
+// 0x0000000D System.Void ARFeatheredPlaneMeshVisualizer::ARPlane_boundaryUpdated(UnityEngine.XR.ARFoundation.ARPlaneBoundaryChangedEventArgs)
+extern void ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F (void);
+// 0x0000000E System.Void ARFeatheredPlaneMeshVisualizer::GenerateBoundaryUVs(UnityEngine.Mesh)
+extern void ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5 (void);
+// 0x0000000F System.Void ARFeatheredPlaneMeshVisualizer::.ctor()
+extern void ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6 (void);
+// 0x00000010 System.Void ARFeatheredPlaneMeshVisualizer::.cctor()
+extern void ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3 (void);
+// 0x00000011 System.Void SpawnableManager::Start()
+extern void SpawnableManager_Start_mFC8BFB2C5F789510DC84AD28B8BF57D00291CDED (void);
+// 0x00000012 System.Void SpawnableManager::Update()
+extern void SpawnableManager_Update_m7A9A38650830610C5C81A77D22ABB3A011E23220 (void);
+// 0x00000013 System.Void SpawnableManager::SpawnPrefab(UnityEngine.Vector3)
+extern void SpawnableManager_SpawnPrefab_mF522EF88B8AAB43C88ADC75967DDEE81030F05DB (void);
+// 0x00000014 System.Void SpawnableManager::OnPointerClick(UnityEngine.EventSystems.PointerEventData)
+extern void SpawnableManager_OnPointerClick_m48C1189F09B602D1BEE4A723CB2D057AEAD7E367 (void);
+// 0x00000015 System.Void SpawnableManager::.ctor()
+extern void SpawnableManager__ctor_m5E989BEEF4E8A6A12828E117976ED1043ACAA1ED (void);
+static Il2CppMethodPointer s_methodPointers[21] = 
+{
+	AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A,
+	AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF,
+	AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21,
+	AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E,
+	AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0,
+	AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E,
+	AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4,
+	ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D,
+	ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110,
+	ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA,
+	ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0,
+	ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8,
+	ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F,
+	ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5,
+	ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6,
+	ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3,
+	SpawnableManager_Start_mFC8BFB2C5F789510DC84AD28B8BF57D00291CDED,
+	SpawnableManager_Update_m7A9A38650830610C5C81A77D22ABB3A011E23220,
+	SpawnableManager_SpawnPrefab_mF522EF88B8AAB43C88ADC75967DDEE81030F05DB,
+	SpawnableManager_OnPointerClick_m48C1189F09B602D1BEE4A723CB2D057AEAD7E367,
+	SpawnableManager__ctor_m5E989BEEF4E8A6A12828E117976ED1043ACAA1ED,
+};
+static const int32_t s_InvokerIndices[21] = 
+{
+	5375,
+	4338,
+	5473,
+	5473,
+	5473,
+	5473,
+	8677,
+	5417,
+	4371,
+	5473,
+	5473,
+	5473,
+	4226,
+	4338,
+	5473,
+	8677,
+	5473,
+	5473,
+	4413,
+	4338,
+	5473,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	21,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ AR Project/Assets/SpawnableManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.EventSystems;
+
+public class SpawnableManager : MonoBehaviour
+{
+    [SerializeField]
+    ARRaycastManager m_RaycastManager;
+    List<ARRaycastHit> m_Hits = new List<ARRaycastHit>();
+    [SerializeField]
+    GameObject spawnablePrefab;
+
+    [SerializeField]
+    GameObject EndGamePrefab;
+
+    [SerializeField]
+    GameObject DeleteText;
+
+    Camera arCam;
+    GameObject spawnedObject;
+    int spawnableCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnedObject = null;
+        arCam = GameObject.Find("AR Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 0)
+            return;
+
+        RaycastHit hit;
+        Ray ray = arCam.ScreenPointToRay(Input.GetTouch(0).position);
+
+        if (m_RaycastManager.Raycast(Input.GetTouch(0).position, m_Hits))
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began && spawnedObject == null)
+            {
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject.tag == "Spawnable")
+                    {
+                        spawnedObject = hit.collider.gameObject;
+                        spawnedObject.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+                    }
+                    else
+                    {
+                        SpawnPrefab(m_Hits[0].pose.position);
+                    }
+                }
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Moved && spawnedObject != null)
+            {
+                spawnedObject.transform.position = m_Hits[0].pose.position;
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                spawnedObject = null;
+            }
+        }
+    }
+
+    private void SpawnPrefab(Vector3 spawnPosition)
+    {
+        spawnedObject = Instantiate(spawnablePrefab, spawnPosition, Quaternion.identity);
+        spawnableCount++;
+
+        // Check if there are 5 SpawnableObjects
+        if (spawnableCount == 5)
+        {
+            Instantiate(EndGamePrefab, spawnPosition, Quaternion.identity);
+            Destroy(DeleteText);
+        }
+    }
+}
